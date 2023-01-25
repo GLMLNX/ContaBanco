fd7fd2797d7862835e4a626ae5966be1cc548466
ZentornoLNX <zentornolinux@gmail.com>
2023-01-24 21:14:45 -0300

    job done

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner input = new Scanner(System.in);
+       System.out.println("Por favor, digite o seu Nome !");
+       String nome=input.nextLine();
+       System.out.println("Por favor, digite o número da Conta !");
+       int conta=input.nextInt();
+       System.out.println("Por favor, digite o seu Saldo !");
+       double saldo=input.nextDouble();
+       System.out.println("Por favor, digite o número da Agência !");
+       String agencia=input.next();
+       System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque.");
     }
 }
